2504a9439ac5151dc34c8a39f5f37dd3a076fcb2
Denny Subke <155585746+dennysubke@users.noreply.github.com>
2025-11-07 22:18:28 +0100

    Update config.js

--- denny-cryptpad/config/config.js
+++ denny-cryptpad/config/config.js
@@ -1,13 +1,13 @@
 // ${APP_DATA_DIR}/config/config.js
 module.exports = {
-  httpUnsafeOrigin: 'https://umbrel-2.local',
-  httpSafeOrigin:   'https://umbrel-2.local',
+  httpUnsafeOrigin: 'http://umbrel-2.local',
+  httpSafeOrigin:   'http://umbrel-2.local',
   blockPath: '/cryptpad/block',
   datastorePath: '/cryptpad/datastore',
   filePath: '/cryptpad/files',
   uploadEnabled: true,
   httpAddress: '0.0.0.0',
   httpPort: 3000,
-  useSecureTransport: true,
+  useSecureTransport: false,
   maxUploadSize: 50 * 1024 * 1024,
 };
